a97ee69d3b70703473a5578f895e30f27ecef9c3
qoaqq <129212733+qoaqq@users.noreply.github.com>
2023-07-21 15:47:17 +0700

    fix conflict header.php

--- mvc/views/Admin_Template/block_admin/header.php
+++ mvc/views/Admin_Template/block_admin/header.php
@@ -29,7 +29,7 @@
     </ul>
     <ul class="header-nav ms-3">
       <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-          <div class="avatar avatar-md"><img class="avatar-img" src="/duan1_Nhom12_WD18202/public/admin/assets/img/avatars/8.jpg" alt="user@email.com"></div>
+          <div class="avatar avatar-md"><img class="avatar-img" src="/duan1_Nhom12_WD18202/public/img/<?= $data['users']['anh'] ?>" alt="user@email.com"></div>
         </a>
         <div class="dropdown-menu dropdown-menu-end pt-0">
           <div class="dropdown-header bg-light py-2">
@@ -49,7 +49,7 @@
             </svg> Comments<span class="badge badge-sm bg-warning ms-2">42</span></a>
           <div class="dropdown-header bg-light py-2">
             <div class="fw-semibold">Settings</div>
-          </div><a class="dropdown-item" href="#">
+          </div><a class="dropdown-item" href="http://localhost/duan1_Nhom12_WD18202/admin/admin_profile/<?= $data['users']['id'] ?>">
             <svg class="icon me-2">
               <use xlink:href="/duan1_Nhom12_WD18202/public/admin/vendors/@coreui/icons/svg/free.svg#cil-user"></use>
             </svg> Profile</a><a class="dropdown-item" href="#">
@@ -65,7 +65,7 @@
           <div class="dropdown-divider"></div><a class="dropdown-item" href="#">
             <svg class="icon me-2">
               <use xlink:href="/duan1_Nhom12_WD18202/public/admin/vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
-            </svg> Lock Account</a><a class="dropdown-item" href="#">
+            </svg> Lock Account</a><a class="dropdown-item" href="http://localhost/duan1_Nhom12_WD18202/admin/admin_login/logOutFeature">
             <svg class="icon me-2">
               <use xlink:href="/duan1_Nhom12_WD18202/public/admin/vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
             </svg> Logout</a>
